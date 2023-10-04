908985dfa238f71ecd7bebf25c9fcb4a2e86a481
Your Name <you@example.com>
2023-10-04 10:58:04 -0500

    Working on palist

--- a3.py
+++ a3.py
@@ -254,7 +254,18 @@ def search_pa_list(src: List[str]) -> List[str]:
         a list of answers. Will be ["I don't understand"] if it finds no matches and
         ["No answers"] if it finds a match but no answers
     """
-    pass
+    for pat, act in pa_list:
+        mat = match(pat, src)
+        #print(mat)
+        if mat is not None:
+            answer = act(mat)
+            return answer if answer else ["No Answers"]
+    
+    return ["I don't understand"]
+        #print(pat)
+        #print(src)
+        #print(act)
+    #pass
 
 
 def query_loop() -> None:
